5e61871d4e59a79cdb6c5692c2c285ac1c4018f6
Nicki Stone <nickik@amazon.com>
2020-12-13 16:33:38 -0800

    updated lambda

--- ReceiptValidationLambda/Sources/ReceiptValidationLambda/main.swift
+++ ReceiptValidationLambda/Sources/ReceiptValidationLambda/main.swift
@@ -15,6 +15,7 @@ struct Output: Codable {
 }
 
 Lambda.run { (context, input: APIGateway.Request, callback: @escaping (Result<APIGateway.Response, Error>) -> Void) in
+    print(input)
     let decoder = JSONDecoder()
     //decode the receipt from the body of the api gateway request
     guard let body = input.body else {
@@ -22,16 +23,13 @@ Lambda.run { (context, input: APIGateway.Request, callback: @escaping (Result<AP
         callback(.success(APIGateway.Response(statusCode: .badRequest)))
         return
     }
-    //body is a base 64 encoded string so need to decode first before decoding from json
-    let decodedData = Data(base64Encoded: body)!
-    let decodedString = String(data: decodedData, encoding: .utf8)!
-    
-    let receiptInput = try! decoder.decode(Input.self, from: decodedString)
-    let httpClient = HTTPClient(eventLoopGroupProvider: .createNew)
-    defer { try? httpClient.syncShutdown() }
-
-    let appStoreClient = AppStore.Client(httpClient: httpClient, secret: "abc123")
+    print(body)
     do {
+        let receiptInput = try decoder.decode(Input.self, from: body)
+        let httpClient = HTTPClient(eventLoopGroupProvider: .createNew)
+        defer { try? httpClient.syncShutdown() }
+        let secret = getEnvVariable(name: "APPSECRET")
+        let appStoreClient = AppStore.Client(httpClient: httpClient, secret: secret)
         let receipt = try appStoreClient.validateReceipt(receiptInput.receipt).wait()
         print(receipt)
         let output = Output(isValid: true)
@@ -44,4 +42,11 @@ Lambda.run { (context, input: APIGateway.Request, callback: @escaping (Result<AP
         print(err)
         callback(.failure(err))
     }
+}
+
+func getEnvVariable(name: String) -> String? {
+    if let value = ProcessInfo.processInfo.environment[name] {
+        return value
+    }
+    return nil
 }
